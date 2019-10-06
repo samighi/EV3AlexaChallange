c700a42a4adc123ce4142c685115a633be928348
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-10-06 22:31:49 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -84,13 +84,13 @@ const additionIntentHandler = {
        // speed = Math.max(1, Math.min(100, parseInt(speed)));
       //    Util.putSessionAttribute(handlerInput, 'speed', speed);
 
-    //   const directive = Util.build(endpointId, NAMESPACE, NAME_CONTROL,
-    //         {
-    //             type: 'add',
-    //             direction: 1,
-    //             duration: duration,
-    //             speed: speed
-    //         });
+      const directive = Util.build(endpointId, NAMESPACE, NAME_CONTROL,
+            {
+                type: 'add',
+                direction: 1,
+                duration: duration,
+                speed: speed
+            });
 
 
 
